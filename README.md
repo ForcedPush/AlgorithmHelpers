c36bc8fab555a360f4946016b97b564591fcd4e0
ForcedPush <34317796+ForcedPush@users.noreply.github.com>
2022-06-26 11:33:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AlgorithmHelpers
+
+A description of this package.
---
+++ Sources/AlgorithmHelpers/AlgorithmHelpers.swift
@@ -0,0 +1,6 @@
+public struct AlgorithmHelpers {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
